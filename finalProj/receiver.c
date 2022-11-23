67dadf77507a6676ea887ff067ef5dea0452f209
Cole <cole.baty@gmail.com>
2022-11-22 21:14:47 -0500

    added makefile; tryna make this super easy for whoever's running this

--- finalProj/receiver.c
+++ finalProj/receiver.c
@@ -12,10 +12,16 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+/* 0xe10c in decimal */
 #define PORT "56712"
 
+/* 100 is arbitrary but useful */
 #define MAXBUFLENGTH 100
 
+/* 
+    commmon variables, declared here because i'm lazy
+    and don't want to worry about passing them around
+ */
 int sockfd, numbytes, rv;
 struct addrinfo hints, *servinfo, *p;
 char buf[MAXBUFLENGTH], data;
@@ -31,33 +37,60 @@ void *get_in_addr(struct sockaddr *sa) {
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
+/**
+ * @brief rdt1.0 extract function; assigns the single char
+ * sent through the socket to the data element
+ * 
+ * @param packet 
+ * @param data 
+ */
 void extract(char *packet, char *data) {
     *data = *packet;
 };
 
+/**
+ * @brief 'delivers' the data to the next layer; i.e. prints to screen
+ * 
+ * @param data 
+ */
 void deliver_data(char *data) {
-    printf("listener: packet contains \"%c\"\n", *data);
+    printf("receiver: packet contains \"%c\"\n", *data);
 }
 
+/**
+ * @brief rdt_recv function specified by instructions. bascially a wrapper
+ * for recvfrom() from sys/socket.h. returns '0' to indicate the connection has been closed,
+ * so the sender will need to explicitly send a packet of 0 length to terminate
+ * the connection
+ * 
+ * @return int 
+ */
 int rdt_recv() {
     numbytes = recvfrom(sockfd, buf, MAXBUFLENGTH-1, 0, 
                 (struct sockaddr*) &their_addr, &addr_len);
 
+    /* error handling per resource document */
     if (numbytes == -1) {
         perror("recv");
         exit(1);
     }
 
-    printf("listener: got packet from %s\n", 
-        inet_ntop(their_addr.ss_family, 
-            get_in_addr((struct sockaddr *)&their_addr), 
-            s, sizeof s));
+    /*  legacy code from resource document - uncomment to see ip addr of sender */
+    // printf("receiver: got packet from %s\n", 
+    //     inet_ntop(their_addr.ss_family, 
+    //         get_in_addr((struct sockaddr *)&their_addr), 
+    //         s, sizeof s));
 
-    printf("listener: packet is %d bytes long\n", numbytes);
+    /* uncomment for length of reeived packet */
+    // printf("receiver: packet is %d bytes long\n", numbytes);
+    
+    /* null terminator for c-string compliance. will not work in printf() without null terminator*/
     buf[numbytes] = '\0';
 
+    /* CALL TO RDT1.0 EXTRACT FUNCTION */
     extract(&buf[0], &data);
 
+    /* CALL TO RDT1.0 DELIVER DATA FUNCTION */
     deliver_data(&data);
 
     return numbytes;
@@ -65,6 +98,11 @@ int rdt_recv() {
 
 int main(int argc, char **argv)
 {
+    /*
+        begin initilization code. this comes almost verbatim from the resource doucment 
+        mentioned in the report. initializes data strcutres required for socket programming
+        on Unix systems, as well as some error checking/handling
+     */
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_DGRAM;
@@ -77,13 +115,13 @@ int main(int argc, char **argv)
 
     for (p = servinfo; p != NULL; p = p->ai_next) {
         if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0) {
-            perror("listener: socket");
+            perror("receiver: socket");
             continue;
         }
 
         if (bind(sockfd, p->ai_addr, p->ai_addrlen) < 0) {
             close(sockfd);
-            perror("listener: connect");
+            perror("receiver: connect");
             continue;
         }
 
@@ -91,19 +129,21 @@ int main(int argc, char **argv)
     }
 
     if (p == NULL) {
-        fprintf(stderr, "listener: failed to connect\n");
+        fprintf(stderr, "receiver: failed to connect\n");
         return 2;
     }
 
     freeaddrinfo(servinfo);
 
-    printf("listener: waiting to recv from...\n");
+    printf("receiver: waiting to recv from...\n");
 
     addr_len = sizeof their_addr;
+    /* end initialization code */
 
     /* greek tragedy - all the action happens offstage */
-    while (rdt_recv() > 0);
+    while (rdt_recv() > 0);/* <--- CALL TO RDT1.0 RDT_RECV FUNCTION */
 
+    /* closes socket */
     close(sockfd);
 
     return 0;
--- finalProj/sender.c
+++ finalProj/sender.c
@@ -14,28 +14,54 @@
 #include <sys/wait.h>
 #include <signal.h>
 
+/* 0xe10c in decimal */
 #define PORT "56712"
 
+/* 
+    commmon variables, declared here because i'm lazy
+    and don't want to worry about passing them around
+ */
 int sockfd;
 struct addrinfo hints, *servinfo, *p;
 int rv, numbytes, totalbytes = 0;
 
+/* message to send to receiver - specified by instruction document */
 char *msg_out = "Testing rdt1.0 protocol\0";
 
+/**
+ * @brief mkd_pkt, specified by instruction document. since we're just 
+ * sending one char at a time, this function is not strictly needed, but
+ * since the instructions specify it, here it is
+ * 
+ * @param data 
+ * @return char 
+ */
 char make_pkt(char data) {
     return data;
 };
 
+/**
+ * @brief udt_send function specified by instructions. essentially a wrapper
+ * function for sendto() from sys/socket.h
+ * 
+ * @param packet 
+ */
 void udt_send(char packet) {
     if ((numbytes = sendto(sockfd, &packet, 1, 0, 
             p->ai_addr, p->ai_addrlen)) == -1) {
-        perror("talker: sendto");
+        perror("sender: sendto");
         exit(1);
     }
 
     totalbytes++;
 };
 
+/**
+ * @brief rdt_send() function. calls udt_send() with specified
+ * data.
+ * 
+ * @param data 
+ */
 void rdt_send(char data) {
     char packet = make_pkt(data);
     udt_send(packet);
@@ -43,8 +69,13 @@ void rdt_send(char data) {
 
 int main(int argc, char **argv) {
 
+    /*
+        begin initilization code. this comes almost verbatim from the resource doucment 
+        mentioned in the report. initializes data strcutres required for socket programming
+        on Unix systems, as well as some error checking/handling
+     */
     if (argc != 2) {
-        fprintf(stderr, "usage: talker hostname\n");
+        fprintf(stderr, "usage: sender hostname\n");
         exit(1);
     }
 
@@ -60,31 +91,36 @@ int main(int argc, char **argv) {
     for (p = servinfo; p != NULL; p = p->ai_next) {
         if ((sockfd = socket(p->ai_family, p->ai_socktype,
                 p->ai_protocol)) == -1) {
-            perror("talker: socket");
+            perror("sender: socket");
             continue;
         }
         break;
     }
 
     if (p == NULL) {
-        fprintf(stderr, "talker: failed to bind\n");
+        fprintf(stderr, "sender: failed to bind\n");
         return 2;
     }
+    /* end initiliaztion code */
 
-    printf("sending...");
+    /* BEGIN RDT1.0 SEND LOOP */
     for (int i = 0; i < strlen(msg_out); i++) {
+        printf("sender: sending \"%c\"\n", (char) msg_out[i]);
         rdt_send(msg_out[i]);
-        printf("%c ", (char) msg_out[i]);
         sleep(1);
     }
     printf("\n");
+    /* END RDT1.0 SEND LOOP */
     
 
+    /* legacy code from resource document */
     freeaddrinfo(servinfo);
 
-    printf("talker: sent %d bytes to %s\n", totalbytes, argv[1]);
+    // printf("sender: sent %d bytes to %s\n", totalbytes, argv[1]);
+
     /* send 0-length packet to terminate connection */
     sendto(sockfd, "", 0, 0, p->ai_addr, p->ai_addrlen);
+    /* close socket */
     close(sockfd);
 
     return 0;
